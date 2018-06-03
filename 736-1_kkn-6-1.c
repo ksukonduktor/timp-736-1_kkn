a325cbbf47820b11c8a916097b4d8fd86a961ba3
Конденко <736-1_kkn@timp2018.novalocal>
2018-06-03 13:15:59 +0000

    feature/p6

---
+++ 736-1_kkn-6-1.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node
+{
+        struct node* left;
+        struct node* right;
+        int value;
+        int key;
+} node;
+
+typedef node *tree;
+
+int init(tree *root)
+{
+        *root = NULL;
+        return 0;
+}
+
+_Bool isEmpty(tree root)
+{
+        if (root == NULL) return 1;
+        return 0;
+}
+
+int add(tree *root, int key, int value)
+{
+        tree p = *root;
+        if (*root != NULL) {
+                while (*root != NULL) {
+                        if (key > (*root)->key) {
+                                if ((*root)->right = NULL) {
+                                        (*root)->right = malloc(sizeof(node));
+                                        *root = (*root)->right;
+                                        break;
+
+                                }
+                                *root = (*root)->right;
+                        }
+                        else
+                                if (key < (*root) -> key) {
+                                        if ((*root)->left = NULL) {
+                                                (*root)->left = malloc(sizeof(node));
+                                                *root = (*root)->left;
+                                                break;
+                                        }
+                                        *root = (*root)->left;
+                                }
+                                else {
+                                        (*root)->value = value;
+                                        return 0;
+                                }
+                }
+        }
+        else {
+                *root = malloc(sizeof(node)); p = *root;
+        }
+        (*root)->left = NULL;
+        (*root)->right = NULL;
+        (*root)->key = key;
+        (*root)->value = value;
+        return 0;
+}
+int find(tree root, int key) {
+        tree p = root; int lvl = 0;
+        if (!isEmpty(root)) {
+                while (p->key != key) {
+                        if (p->key > key) p = p->left;
+                        else if (p->key != key) p = p->right;
+                        lvl++;
+                        if (p == NULL) return -1;
+                }
+                printf("%d", lvl);
+                return p->value;
+
+        }
+        return -1;
+
+}
+
+int destroy(tree *root) {
+        if ((*root)->left != NULL) destroy(&((*root)->left));
+        if ((*root)->right != NULL) destroy(&((*root)->right));
+        if (((*root)->left == NULL) && ((*root)->right == NULL)); {
+                free(*root);
+                *root = NULL;
+                return 0;
+        }
+}
+void inorder(node *root)
+{
+    if (root == NULL)
+        return;
+    inorder(root -> left);
+    if (root -> key)
+        printf("%d ", root -> key);
+    inorder(root -> right);
+}
+int main()
+{
+        tree t;
+        int n,i,abc,k,v;
+        init(&t);
+        scanf("%d",&n);
+        for(i=0; i<n; i++){
+                scanf("%d %d",&k,&v);
+                add(&t,k,v);
+        }
+        for(i=0; i<3; i++) {
+                scanf("%d",&abc);
+                printf("%d\n", find(t,abc));
+        }
+        inorder(t);
+        destroy(&t);
+        return 0;
+}
